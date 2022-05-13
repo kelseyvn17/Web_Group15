05ffe16628167eaf22da6b8ed17ea5762169f99f
Fivgarro <89268595+Fivgarro@users.noreply.github.com>
2022-05-13 22:14:28 +0700

    Update form_valid.php
    finish email validation

--- form_valid.php
+++ form_valid.php
@@ -1,4 +1,5 @@
 //https://www.simplilearn.com/tutorials/php-tutorial/php-form-validation//
+//https://www.codespeedy.com/save-html-form-data-in-a-txt-text-file-in-php///
 
 <!DOCTYPE HTML>  
 
@@ -9,6 +10,55 @@
     </style>
 </head>
 <body>  
+    
+/////////////////////Bring this to SignUp/////////////////////
+    ///email validation///
+    <form name="form1" action="#"> 
+Email: <input type='text' name='email'/></br></br>
+<input type="submit" name="submit" value="Submit" onclick="validateEmail(document.form1.email)"/>
+</form>
+
+    <script> 
+        function validateEmail(emailId) {
+            var mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+            if(emailId.value.match(mailformat)) {
+                document.form1.text1.focus();
+                return true;
+        } else {
+            alert("Invalid email address.");
+            document.form1.text1.focus();
+            return false;
+        }
+        }
+        function
+    </script>
+    
+    ///store data with name=""///
+    <?php
+              if(isset($_POST['fname','lname','email','password','file'])) {
+                  $fname=$_POST['fname'];
+                  $lname=$_POST['lname'];
+                  $email=$_POST['email'];
+                  $pass=$_POST['password'];
+                  $password= password_hash($pass,PASSWORD_DEFAULT);
+                  $img=$_POST['file'];
+                  $fp = fopen('accounts.db', 'a');
+                  fwrite($fp, $fname,$lname,$email,$password,$img);
+                  fclose($fp);
+                  if ($email == '') {
+                      echo $email_error = '<span style="color:red">You must enter your email.</span>';
+                      return false;
+                  } else {
+                      $email_error = '';
+                  }
+              }
+    ?>
+    
+    
+    
+/////////////////////////////
+    
+    
     <?php
         $nameErr = $emailErr = $genderErr = $websiteErr = "";
         $name = $email = $gender = $comment = $website = "";
@@ -22,44 +72,16 @@
             $nameErr = "Only letters and white space allowed";
             }
         }
-        if (empty($_POST["email"])) {
-            $emailErr = "valid Email address";
-        } else {
-            $email = test_input($_POST["email"]);
-            // check if e-mail address is well-formed
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $emailErr = "The email address is incorrect";
-            }
-        }  
-        if (empty($_POST["website"])) {
-            $website = "";
-        } else {
-            $website = test_input($_POST["website"]);
-            // check if URL address syntax is valid
-            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-            $websiteErr = "Enter a valid Webiste URL";
-            }    
-        }
-        if (empty($_POST["comment"])) {
-            $comment = "";
-        } else {
-            $comment = test_input($_POST["comment"]);
-        }        
-        if (empty($_POST["gender"])) {
-            $genderErr = "Please select a gender";
-        } else {
-            $gender = test_input($_POST["gender"]);
-        }
-        }
+        
         function test_input($data) {
         $data = trim($data);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
         return $data;
         }
-
     ?>
 
+////////////////////    
     <h2>PHP Form Validation Example</h2>
     <p><span class="error">* required field</span></p>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
@@ -81,19 +103,9 @@
         <br><br>
         <input type="submit" name="submit" value="Submit">  
     </form>
-    <?php
-        echo "<h2> Final Output:</h2>";
-        echo $name;
-        echo "<br>";
-        echo $email;
-        echo "<br>";
-        echo $website;
-        echo "<br>";
-        echo $comment;
-        echo "<br>";
-        echo $gender;
-    ?>
-
+ //////////////////////
+ 
+    
 </body>
 
 </html>
