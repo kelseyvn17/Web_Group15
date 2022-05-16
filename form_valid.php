941a197ee98f9dd7291e5e5173c35c888b1fa0ee
Fivgarro <89268595+Fivgarro@users.noreply.github.com>
2022-05-16 16:18:23 +0700

    Update form_valid.php

--- form_valid.php
+++ form_valid.php
@@ -1,15 +1,7 @@
 
 //https://www.codespeedy.com/save-html-form-data-in-a-txt-text-file-in-php///
 
-<!DOCTYPE HTML>  
 
-<html>
-<head>
-    <style>
-        .error {color: #FF0000;}
-    </style>
-</head>
-<body>  
     
 /////////////////////Bring this to SignUp/////////////////////
     
@@ -68,55 +60,4 @@
     
     
 /////////////////////////////
-    
-    
-    <?php
-        $nameErr = $emailErr = $genderErr = $websiteErr = "";
-        $name = $email = $gender = $comment = $website = "";
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["name"])) {
-            $nameErr = "Please enter a valid name";
-        } else {
-            $name = test_input($_POST["name"]);
-            // check if name only contains letters and whitespace
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-            $nameErr = "Only letters and white space allowed";
-            }
-        }
-        
-        function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-        }
-    ?>
 
-////////////////////    
-    <h2>PHP Form Validation Example</h2>
-    <p><span class="error">* required field</span></p>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-        FullName: <input type="text" name="name">
-        <span class="error">* <?php echo $nameErr;?></span>
-        <br><br>
-        E-mail address: <input type="text" name="email">
-        <span class="error">* <?php echo $emailErr;?></span>
-        <br><br>
-        Website: <input type="text" name="website">
-        <span class="error"><?php echo $websiteErr;?></span>
-        <br><br>
-        Comment: <textarea name="comment" rows="2" cols="10"></textarea>
-        <br><br>
-        Gender:
-        <input type="radio" name="gender" value="female">Female
-        <input type="radio" name="gender" value="male">Male
-            <span class="error">* <?php echo $genderErr;?></span>
-        <br><br>
-        <input type="submit" name="submit" value="Submit">  
-    </form>
- //////////////////////
- 
-    
-</body>
-
-</html>
