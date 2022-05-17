46154a9403e1797af9bca42bab81f4fdea38e746
kelseyvn17 <86825876+kelseyvn17@users.noreply.github.com>
2022-05-17 16:04:13 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,13 +1,19 @@
-function validate()
-{
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    if (username == 'admin' && password == 'admin123')
-    {
-        window.location="cookie.html";
+var attempt = 3;
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ( username == "admin" && password == "admin123"){
+        window.location = "admin_dashboard.html";
         return false;
     }
     else{
-        alert("login failed");
+        attempt --;
+        alert("Failed Login!");
+    if( attempt == 0){
+        document.getElementById("username").disabled = true;
+        document.getElementById("password").disabled = true;
+        document.getElementById("submit").disabled = true;
+        return false;
     }
+}
 }
